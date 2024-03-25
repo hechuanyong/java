b22dab694835f9b993d851b25eb5eab7995938bc
chen <zhangsan@itcast.cn>
2024-03-25 22:23:30 +0800

    我的第一个java程序

---
+++ java-foundational/src/main/java/net/ittimeline/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.ittimeline.java.foundational.object;
+
+/**
+ * 我的第一个java程序
+ *
+ * @author tony 254280671@qq.com
+ * @version 2024/3/25 22:16
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world with windows10 & JDK11& git & intellij IDEA ");
+    }
+}
